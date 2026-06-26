a1fc2227c44be4a8b0fa139532045cdd24cfe82e
FlorijanDem <florijan.dem@gmail.com>
2026-06-26 14:55:52 +0300

    Add basic lesson

---
+++ firstLesson.py
@@ -0,0 +1,14 @@
+line = "Zen of Python, by Tim Peters"
+
+x1 = line.split()
+print(x1[1][1])
+print(x1[2][0])
+print(x1[:1])
+print(x1[-1::]) 
+print(x1[::-1])
+
+for i in x1:
+    print(i)
+
+x2 = line.replace("Python", "Programming")
+print(x2)
