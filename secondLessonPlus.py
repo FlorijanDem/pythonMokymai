e9fe30f620deb4f3d2c064a144087f3af22f5792
FlorijanDem <florijan.dem@gmail.com>
2026-07-03 15:03:11 +0300

    Add persona code generator

--- secondLessonPlus.py
+++ secondLessonPlus.py
@@ -1,4 +1,4 @@
-def check_person_code(code):
+def check_person_code(code, have_validation_code = True):
     first_number = int(code[0])
     second_number = int(code[1])
     st3_number = int(code[2])
@@ -9,8 +9,12 @@ def check_person_code(code):
     st8_number = int(code[7])
     st9_number = int(code[8])
     st10_number = int(code[9])
-    validation_number = int(code[10])
     
+    if(have_validation_code == True):
+        validation_number = int(code[10])
+    elif(have_validation_code == False):
+        validation_number:int
+
     if first_number < 0 or first_number > 6:
         return False
     
@@ -28,7 +32,9 @@ def check_person_code(code):
 
     remainder = sum % 11
 
-    if (remainder != 10):
+    if (have_validation_code == False and remainder != 10):
+        return remainder
+    elif (remainder != 10):
         if (remainder == validation_number):
             return True
         
@@ -51,4 +57,46 @@ def check_person_code(code):
     else:
         return False
 
-print(check_person_code("46806013013"))
+# print(check_person_code("46806013013"))
+
+def generate_person_code(gender, birth_date, line):
+    
+    date_split = birth_date.split("-")
+    first_number:int
+    print(int(date_split[0]))
+
+    if int(date_split[0]) >= 1801 and int(date_split[0]) < 1900:
+        if gender == "male":
+            first_number = 1
+        elif gender == "woman":
+            first_number = 2
+    elif int(date_split[0]) >= 1901 and int(date_split[0]) < 2000:
+        if gender == "male":
+            first_number = 3
+        elif gender == "woman":
+            first_number = 4
+    elif int(date_split[0]) >= 2001 and int(date_split[0]) < 2100:
+        if gender == "male":
+            first_number = 5
+        elif gender == "woman":
+            first_number = 6
+    else:
+        return "Wrong date range"
+    
+    second_number = int(date_split[0][2])
+    st3_number = int(date_split[0][3])
+    st4_number = int(date_split[1][0])
+    st5_number = int(date_split[1][1])
+    st6_number = int(date_split[2][0])
+    st7_number = int(date_split[2][1])
+    st8_number = int(line[0])
+    st9_number = int(line[1])
+    st10_number = int(line[2])
+    
+    persona_code_without_validation = f"{first_number}{second_number}{st3_number}{st4_number}{st5_number}{st6_number}{st7_number}{st8_number}{st9_number}{st10_number}"
+    validation_number = check_person_code(code = persona_code_without_validation, have_validation_code = False)
+    persona_code_final = f"{persona_code_without_validation}{validation_number}"
+
+    return persona_code_final
+
+print(generate_person_code(gender = "male", birth_date = "2020-01-01", line = "300"))
